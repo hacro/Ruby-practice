fb47c94a80d6204a0755014534e2322b31e388f6
hazuki <87082685+hacro@users.noreply.github.com>
2023-09-11 23:35:41 +0900

    繰り返し分などの応用問題を行いました

--- compare.rb
+++ compare.rb
@@ -5,6 +5,11 @@
 #     puts "合計は200よりも多いです"
 # end
 
-puts "何かを入力"
-input_key = gets
-puts "入力内容は#{input_key}です"
+# puts "何かを入力"
+# input_key = gets
+# puts "入力内容は#{input_key}です"
+
+amounts = {"りんご"=>2, "いちご"=>5, "オレンジ"=>3}
+amounts.each do |fruit, amount|
+    puts "#{fruit}は#{amount}個です。"
+end
---
+++ lesson.rb
@@ -0,0 +1,10 @@
+puts "計算を始めます\nふたつの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+num = a*b
+
+puts "計算結果を出力します"
+
+puts "a*b= #{num}"
